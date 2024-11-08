5dc40f6eec1de0ba86a7934ae36255beb152521e
Andrés Martínez <autummata@gmail.com>
2024-11-08 16:23:44 -0600

    Add: Chat functionality, basic design, connection to Supabase DB

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,19 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DarkTheme as NavigationDarkTheme, DefaultTheme as NavigationDefaultTheme } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { Provider as PaperProvider, MD3DarkTheme as PaperDarkTheme, DefaultTheme as PaperDefaultTheme } from 'react-native-paper';
+import merge from 'deepmerge';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+const CombinedDefaultTheme = merge(NavigationDefaultTheme, PaperDefaultTheme);
+const CombinedDarkTheme = merge(NavigationDarkTheme, PaperDarkTheme);
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -27,11 +32,11 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <PaperProvider theme={colorScheme === 'dark' ? CombinedDarkTheme : CombinedDefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-    </ThemeProvider>
+    </PaperProvider>
   );
 }
